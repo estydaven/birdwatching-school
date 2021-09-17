b9089fb3a78cd032e61d58f695b79bd8ae37cbec
estydaven <esetnod1111@gmail.com>
2021-09-17 17:36:31 +0300

    deploy

---
+++ precache-manifest.265d2a0bc74e0eae160fcb62d6b9db27.js
@@ -0,0 +1,234 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b5828eb16abe52636bd",
+    "url": "/birdwatching-school/css/app.f096c583.css"
+  },
+  {
+    "revision": "83019c8e0bacc2e7fd53",
+    "url": "/birdwatching-school/css/chunk-20000e91.c06cefbe.css"
+  },
+  {
+    "revision": "2ef07b92ffcbffc9c86f",
+    "url": "/birdwatching-school/css/chunk-2caae369.435888f3.css"
+  },
+  {
+    "revision": "439a68c92d988cd0c2d6",
+    "url": "/birdwatching-school/css/chunk-6585aec4.87ab731c.css"
+  },
+  {
+    "revision": "f91121e4e543414f6af6",
+    "url": "/birdwatching-school/css/chunk-7097b3fc.a19b0706.css"
+  },
+  {
+    "revision": "64717d2c6821c0c451f636f0e406f4a4",
+    "url": "/birdwatching-school/fonts/comfortaa-v30-cyrillic-700.64717d2c.woff2"
+  },
+  {
+    "revision": "be48ecb03fe9b2cb3fafa280ecf33eb1",
+    "url": "/birdwatching-school/fonts/comfortaa-v30-cyrillic-700.be48ecb0.woff"
+  },
+  {
+    "revision": "b5e4305e2802acfa8c432e8ef9469307",
+    "url": "/birdwatching-school/fonts/comfortaa-v30-cyrillic-regular.b5e4305e.woff"
+  },
+  {
+    "revision": "f57e774d707ebb4ba8192c9f2d16e201",
+    "url": "/birdwatching-school/fonts/comfortaa-v30-cyrillic-regular.f57e774d.woff2"
+  },
+  {
+    "revision": "5b3e29b01c0db247cf11e70f8e4aabd6",
+    "url": "/birdwatching-school/fonts/pacifico-v17-cyrillic-regular.5b3e29b0.woff2"
+  },
+  {
+    "revision": "d245df437fc572572b38635740eed0fa",
+    "url": "/birdwatching-school/fonts/pacifico-v17-cyrillic-regular.d245df43.woff"
+  },
+  {
+    "revision": "4f24cc8b7e30236cf9d74e528734beab",
+    "url": "/birdwatching-school/img/bg1.4f24cc8b.jpg"
+  },
+  {
+    "revision": "83d7f418c4717c6898983ed89a0bc464",
+    "url": "/birdwatching-school/img/bg2.83d7f418.jpg"
+  },
+  {
+    "revision": "e1fae9c3cb46886d004e52d457031d64",
+    "url": "/birdwatching-school/img/bg2.e1fae9c3.jpg"
+  },
+  {
+    "revision": "5ce809ae650d501aef3172298511c590",
+    "url": "/birdwatching-school/img/bg4.5ce809ae.jpg"
+  },
+  {
+    "revision": "9de9caf54c616a3f10f473e003fe099a",
+    "url": "/birdwatching-school/img/bg5.9de9caf5.jpg"
+  },
+  {
+    "revision": "cd83eb2d034485ea121b7774cf8d44fb",
+    "url": "/birdwatching-school/img/bg6.cd83eb2d.jpg"
+  },
+  {
+    "revision": "1d985abf799462efb60f1cee3d0013eb",
+    "url": "/birdwatching-school/img/bird1.1d985abf.png"
+  },
+  {
+    "revision": "28c8a64d08334b8cd04a8696c3588fd8",
+    "url": "/birdwatching-school/img/bird2.28c8a64d.png"
+  },
+  {
+    "revision": "3fe3d7862845a042583187e1ac1abbc7",
+    "url": "/birdwatching-school/img/bird3.3fe3d786.png"
+  },
+  {
+    "revision": "1bb39eca465ef098763703cbe0dc2c85",
+    "url": "/birdwatching-school/img/bird4.1bb39eca.png"
+  },
+  {
+    "revision": "9ee1a3da50b0bf5a5e81f7bee94d90dc",
+    "url": "/birdwatching-school/img/bird5.9ee1a3da.png"
+  },
+  {
+    "revision": "defae1b9a76f9eb90c24ed3b34793b52",
+    "url": "/birdwatching-school/img/bird6.defae1b9.png"
+  },
+  {
+    "revision": "af6e1ff287ad1deec198630582d044f2",
+    "url": "/birdwatching-school/img/bird7.af6e1ff2.png"
+  },
+  {
+    "revision": "7f032f538ab08fdfd996b7bac3a0f999",
+    "url": "/birdwatching-school/img/bird8.7f032f53.png"
+  },
+  {
+    "revision": "702166de0bb4719e3496cdc556d0193c",
+    "url": "/birdwatching-school/img/bird9.702166de.png"
+  },
+  {
+    "revision": "b85bfe39708c61feedeeac3f6a42afec",
+    "url": "/birdwatching-school/img/how-image.b85bfe39.jpg"
+  },
+  {
+    "revision": "facb143f3638dbace351f3fa7559e0fb",
+    "url": "/birdwatching-school/img/img1.facb143f.jpg"
+  },
+  {
+    "revision": "8cfefada9112449e4d456eba99f71f2a",
+    "url": "/birdwatching-school/img/img10.8cfefada.jpg"
+  },
+  {
+    "revision": "149e49ff93f12ef36730a89cb3da62c8",
+    "url": "/birdwatching-school/img/img11.149e49ff.jpg"
+  },
+  {
+    "revision": "8d6d81c7474bd1e5043099c5ef26bd1c",
+    "url": "/birdwatching-school/img/img12.8d6d81c7.jpg"
+  },
+  {
+    "revision": "50720bf34b566730c04078da8e91c501",
+    "url": "/birdwatching-school/img/img13.50720bf3.jpg"
+  },
+  {
+    "revision": "289e6a345027e684e624956e59aa72d6",
+    "url": "/birdwatching-school/img/img14.289e6a34.jpg"
+  },
+  {
+    "revision": "6579786703182d9f11ca0b8265e4b626",
+    "url": "/birdwatching-school/img/img2.65797867.jpg"
+  },
+  {
+    "revision": "d0e3bb4c5c28d6ee075651e0fecef2cc",
+    "url": "/birdwatching-school/img/img3.d0e3bb4c.jpg"
+  },
+  {
+    "revision": "0ea002f559a33056efa1bf17a4789c9b",
+    "url": "/birdwatching-school/img/img4.0ea002f5.jpg"
+  },
+  {
+    "revision": "bbd09f4e5a2ae5d12985bb66545cb319",
+    "url": "/birdwatching-school/img/img5.bbd09f4e.jpg"
+  },
+  {
+    "revision": "0a73b2a894449a5d611d80cee58dfe33",
+    "url": "/birdwatching-school/img/img6.0a73b2a8.jpg"
+  },
+  {
+    "revision": "e266a1337a9678d385c910c30d7788db",
+    "url": "/birdwatching-school/img/img7.e266a133.jpg"
+  },
+  {
+    "revision": "1a1c43cf44a49e4791a4a32495f813ca",
+    "url": "/birdwatching-school/img/img8.1a1c43cf.jpg"
+  },
+  {
+    "revision": "112f199d5bdfe546829d9cc9c1618efd",
+    "url": "/birdwatching-school/img/img9.112f199d.jpg"
+  },
+  {
+    "revision": "410bdbf1e520ea6dc8f0fb4004b18ee9",
+    "url": "/birdwatching-school/img/logo.410bdbf1.png"
+  },
+  {
+    "revision": "8924068422137f1c60664f697c95c23c",
+    "url": "/birdwatching-school/img/man.89240684.jpg"
+  },
+  {
+    "revision": "fa689aa6e1e68c4891bc04aaa88c22ef",
+    "url": "/birdwatching-school/img/mark.fa689aa6.svg"
+  },
+  {
+    "revision": "b3400dd5610d7d92f835e204a6e604c1",
+    "url": "/birdwatching-school/img/school.b3400dd5.jpg"
+  },
+  {
+    "revision": "a58002aeda2d8e2bf4943520dcdf68b6",
+    "url": "/birdwatching-school/img/teacher.a58002ae.jpg"
+  },
+  {
+    "revision": "20e93238622293cf91e9d147ae4145ef",
+    "url": "/birdwatching-school/img/thought.20e93238.svg"
+  },
+  {
+    "revision": "5e042137cd6076030b35c563f75e9d07",
+    "url": "/birdwatching-school/img/woman.5e042137.jpg"
+  },
+  {
+    "revision": "bf4f9326bb4436fd8945bc0be2488c18",
+    "url": "/birdwatching-school/img/wood.bf4f9326.jpg"
+  },
+  {
+    "revision": "60e6c80d56ff4c3e43f546a8431072ba",
+    "url": "/birdwatching-school/index.html"
+  },
+  {
+    "revision": "5b5828eb16abe52636bd",
+    "url": "/birdwatching-school/js/app.a25f66ff.js"
+  },
+  {
+    "revision": "83019c8e0bacc2e7fd53",
+    "url": "/birdwatching-school/js/chunk-20000e91.a5147cf8.js"
+  },
+  {
+    "revision": "2ef07b92ffcbffc9c86f",
+    "url": "/birdwatching-school/js/chunk-2caae369.71417a67.js"
+  },
+  {
+    "revision": "439a68c92d988cd0c2d6",
+    "url": "/birdwatching-school/js/chunk-6585aec4.40bd91d5.js"
+  },
+  {
+    "revision": "f91121e4e543414f6af6",
+    "url": "/birdwatching-school/js/chunk-7097b3fc.846532de.js"
+  },
+  {
+    "revision": "85589076798db3aaed82",
+    "url": "/birdwatching-school/js/chunk-vendors.c5768a79.js"
+  },
+  {
+    "revision": "c92b4d03efca9368ac5b94da8e9bb503",
+    "url": "/birdwatching-school/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/birdwatching-school/robots.txt"
+  }
+]);
